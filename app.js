2f94657dba9cf857a9c7e6a0395c0e0ad6f17124
maggiey8 <144460257+maggiey8@users.noreply.github.com>
2023-10-10 10:20:35 -0400

    Create app.js

---
+++ app.js
@@ -0,0 +1,23 @@
+const chara = [
+    {
+    firstName: "Mary",
+    lastName: "Sue",
+    age: 19,
+    status: "Alive",
+    },
+    {
+    firstName: "Mary",
+    lastName: "Sue",
+    age: 19,
+    status: "Alive",
+    },
+    {
+    firstName: "Mary",
+    lastName: "Sue",
+    age: 19,
+    status: "Alive",
+    },
+];
+
+
+chara.forEach((el) => console.log(`${el.firstName} ${el.lastName}`));
